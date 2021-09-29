25802b99d4578f0b25e04a9aa50f0543e47e9576
Patrice Ahamed <patrice.ahamed@gmail.com>
2021-09-29 21:44:05 +0200

    Affichage produits page d'accueil

--- index_test.js
+++ index_test.js
@@ -1,41 +1,39 @@
 
 async function call() {
 
-  fetch("http://localhost:3000/api/teddies")
+  let response = await fetch("http://localhost:3000/api/teddies")
   
-  .then (function(response) {
     if (response.ok) {
     return response.json()    
-    }    
-  })
+    }   
 
-  .catch (function (erreur) {
+    else {
       throw "Erreur sur la requête"
-    })
+    }
+
 }
 
 async function bearProducts(){
   let products = await call()
+  console.log(products)
+  return products
 }
 
 bearProducts()
   .then(function(res) {
-    document
-      .getElementById("bear")
-      .innerText = ''
 
-    for (let product of products) {
-      let row = document.createElement('div')
-      row.className = "row"
+    let article = document.getElementById("bear")
+    article.innerText = ''
 
+    for (let product of res) {
       let col = document.createElement('div')
       col.className = "col-12 col-lg-4"
 
       let card = document.createElement('div')
-      col.className = "col-12 col-lg-4"
+      card.className = "my-5 mb-3"
 
       let a = document.createElement('a')
-      a.href="${product.name}.html"
+      a.href=`${product.name}.html`
       a.className = "stretched-link"
 
       let img = document.createElement('img')
@@ -48,86 +46,27 @@ bearProducts()
 
       let cardTitle = document.createElement('h5')
       cardTitle.className = "card-title text-center"
-      cardTitle.innerText = "${product.name}"
+      cardTitle.innerText = `${product.name}`
 
       let cardText = document.createElement('p')
       cardText.className = "card-text text-center"
-      cardTitle.innerText = "${product.description}"
+      cardText.innerText = `${product.description}`
 
       let priceBear = document.createElement('h3')
       priceBear.className = "cart-title text-center font-weight-bold text-danger"
-      cardTitle.innerText = "${product.price}"
+      priceBear.innerText = `${product.price}`
 
-      section.appendChild(row)
-      row.appendChild (col)
+      article.appendChild (col)
       col.appendChild (card)
       card.appendChild (a)
       a.appendChild (img)
-      cardBody.appendChild (card)
-      cardTitle.appendChild (cardBody)
-      cardText.appendChild (cardBody)
-      priceBear.appendChild (cardBody)
+      card.appendChild (cardBody)
+      cardBody.appendChild (cardTitle)
+      cardBody.appendChild (cardText)
+      cardBody.appendChild (priceBear)
 
-      console.log(row)
+      console.log(article)
 
       }
 });
-
-// bearProducts()
-//   .then(function(res){
-//     console.log(products)
-
-//     // let section = document.getElementById('bear')
-//     // section.innerText = ''
-
-//   // for (let product of products) {
-//   //   let row = document.createElement('div')
-//   //   row.className = "row"
-
-//   //   let col = document.createElement('div')
-//   //   col.className = "col-12 col-lg-4"
-
-//   //   let card = document.createElement('div')
-//   //   col.className = "col-12 col-lg-4"
-
-//   //   let a = document.createElement('a')
-//   //   a.href="${product.name}.html"
-//   //   a.className = "stretched-link"
-
-//   //   let img = document.createElement('img')
-//   //   img.className = "card-img-top"
-//   //   img.src = product.imageUrl
-//   //   img.alt = product.className
-
-//   //   let cardBody = document.createElement('div')
-//   //   cardBody.className = "card-body mx-auto"
-
-//   //   let cardTitle = document.createElement('h5')
-//   //   cardTitle.className = "card-title text-center"
-//   //   cardTitle.innerText = "${product.name}"
-
-//   //   let cardText = document.createElement('p')
-//   //   cardText.className = "card-text text-center"
-//   //   cardTitle.innerText = "${product.description}"
-
-//   //   let priceBear = document.createElement('h3')
-//   //   priceBear.className = "cart-title text-center font-weight-bold text-danger"
-//   //   cardTitle.innerText = "${product.price}"
-
-//   //   section.appendChild(row)
-//   //   row.appendChild (col)
-//   //   col.appendChild (card)
-//   //   card.appendChild (a)
-//   //   a.appendChild (img)
-//   //   cardBody.appendChild (card)
-//   //   cardTitle.appendChild (cardBody)
-//   //   cardText.appendChild (cardBody)
-//   //   priceBear.appendChild (cardBody)
-
-//   //   console.log(section)
-
-//     // }
-
-//   });
-
 
